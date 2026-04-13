f38a9ca51ea969265e06d6c5f29ce1585330e059
Chris <ST10520293@rcconnect.edu.za>
2026-04-13 19:35:42 +0200

    initial commit

--- POEPart1/src/LoginTest.java
+++ POEPart1/src/LoginTest.java
@@ -1,48 +1,71 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-    import org.junit.Test;
-    import static org.junit.Assert.*;
-    //Test class used to verify that validation methods work correctly
-    public class LoginTest {
+public class LoginTest {
+
 
     @Test
-    public void testCheckUsername(){
+    public void testCheckUsername() {
         Login login = new Login();
 
-        login.username="kyl_1";
-        assertTrue(login.checkUsername("kyl_1"));
+        // Valid username: contains underscore and 5 or fewer characters
+        login.username = "kyl_1";
+        boolean validUsername = login.checkUsername("kyl_1");
+        assertEquals("Username successfully added", true, validUsername);
+        if (validUsername) {
+            System.out.println("Username successfully added");
+        }
 
-
-        login.username="kyle!!!!!!!";
-        assertFalse(login.checkUsername("kyle!!!!!!!"));
+        // Invalid username: too long and no underscore
+        login.username = "kyle!!!!!!!";
+        boolean invalidUsername = login.checkUsername("kyle!!!!!!!");
+        assertEquals("Username does not meet requirements", false, invalidUsername);
+        if (!invalidUsername) {
+            System.out.println("Username does not meet requirements");
+        }
     }
+
     @Test
     public void testCheckPasswordComplexity() {
-        Login login = new Login ();
+        Login login = new Login();
 
+        // Valid password: 8+ chars, uppercase, digit, special character
         login.password = "Ch&&sec@ke99!";
-        assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
+        boolean validPassword = login.checkPasswordComplexity("Ch&&sec@ke99!");
+        assertEquals("Password successfully added", true, validPassword);
+        if (validPassword) {
+            System.out.println("Password successfully added");
+        }
 
+        // Invalid password: too simple
         login.password = "password";
-        assertFalse(login.checkPasswordComplexity("password"));
+        boolean invalidPassword = login.checkPasswordComplexity("password");
+        assertEquals("Password does not meet requirements", false, invalidPassword);
+        if (!invalidPassword) {
+            System.out.println("Password does not meet requirements");
+        }
     }
+
     @Test
     public void testCheckCellPhoneNumber() {
         Login login = new Login();
 
+        // Valid South African phone number: starts with +27 followed by 10 digits
         login.PhoneNumber = "+27838968976";
-        assertTrue(login.checkCellPhoneNumber());
+        boolean validPhone = login.checkCellPhoneNumber();
+        assertEquals("Phone number successfully added", true, validPhone);
+        if (validPhone) {
+            System.out.println("Phone number successfully added");
+        }
 
+        // Invalid phone number: wrong format
         login.PhoneNumber = "08966553";
-        assertFalse(login.checkCellPhoneNumber());
+        boolean invalidPhone = login.checkCellPhoneNumber();
+        assertEquals("Phone number does not meet requirements", false, invalidPhone);
+        if (!invalidPhone) {
+            System.out.println("Phone number does not meet requirements");
+        }
     }
-    @Test
-    public void testLoginUser() {
-        Login login = new Login();
-        login.username = "kyl_1";
-        login.password = "Ch&&sec@ke99!";
 
-        assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!"));
-        assertFalse(login.loginUser("kyl_1", "wrongpassword"));
-    }
-    //Used for Debugging and ensuring methods return expected results
+
 }
