e572ca8656860f91be96073a8386fb9e471bf68a
Chris <ST10520293@rcconnect.edu.za>
2026-04-13 12:05:32 +0200

    initial commit

--- POEPart1/src/Login.java
+++ POEPart1/src/Login.java
@@ -36,7 +36,7 @@ public class Login {
         return enteredUsername.equals(username) && enteredPassword.equals(password);
 
     }
-    //Method that handles the full user registration processss
+    //Method that handles the full user registration process
     public String Register() {
 
         //Prompt to enter users first and last name
